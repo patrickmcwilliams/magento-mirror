acd5c3dba6267a8cb0477f770e329eb8fc574531
patrickmcwilliams <patrickmcwilliams@Patricks-MacBook-Pro-2.local>
2015-01-05 14:30:07 -0800

    added tealium module 2.0

---
+++ app/code/local/Tealium/Tags/Helper/Data.php
@@ -0,0 +1,106 @@
+<?php
+require_once (Mage::getBaseDir ( 'lib' ) . '/Tealium/Tealium.php');
+
+class Tealium_Tags_Helper_Data extends Mage_Core_Helper_Abstract {
+	protected $tealium;
+	public function init(&$store, &$page = array(), $pageType) {
+
+		$account = $this->getAccount ( $store );
+		$profile = $this->getProfile ( $store );
+		$env = $this->getEnv ( $store );
+		
+		$data = array (
+				"store" => $store,
+				"page" => $page 
+		);
+		$this->tealium = new Tealium ( $account, $profile, $env, $pageType, $data );
+		
+		return $this;
+	}
+	
+	public function addCustomDataFromSetup(&$store, $pageType){
+		if (Mage::getStoreConfig ( 'tealium_tags/general/udo_enable', $store )) {
+			@include_once (Mage::getStoreConfig ( 'tealium_tags/general/udo', $store ));
+
+			if ( method_exists($this, "getCustomUdo") ){
+				$customUdoElements = getCustomUdo();
+				if ( is_array($customUdoElements) && isAssocArray($customUdoElements) ){
+					$udoElements = $customUdoElements;
+				}
+			}
+			elseif (!isset($udoElements) || ( isset($udoElements) && !isAssocArray($udoElements) )){
+				$udoElements = array();
+			}
+			
+			if ( isset($udoElements[$pageType]) ){
+				$this->tealium->updateUdo($udoElements[$pageType]);
+			}
+			
+		}
+		
+		return $this;
+	}
+	
+	public function addCustomDataFromObject($udoObject){
+		if ( is_array($udoObject) && isAssocArray($udoObject) ){
+			$this->tealium->updateUdo($udoObject);
+		}
+		return $this;
+	}
+	
+	protected static function isAssocArray( $array) {
+		$keys = array_keys($array);
+		return array_keys($keys) !== $keys;
+	}
+	
+	public function isEnabled($store) {
+		return Mage::getStoreConfig ( 'tealium_tags/general/enable', $store );
+	}
+	public function enableOnePageCheckout($store) {
+		return Mage::getStoreConfig ( 'tealium_tags/general/onepage', $store );
+	}
+	public function externalUdoEnabled($store) {
+		return Mage::getStoreConfig ( 'tealium_tags/general/udo_enable', $store );
+	}
+	public function getTealiumBaseUrl($store) {
+		$account = $this->getAccount ( $store );
+		$profile = $this->getProfile ( $store );
+		$env = $this->getEnv ( $store );
+		return "//tags.tiqcdn.com/utag/$account/$profile/$env/utag.js";
+	}
+	public function getTealiumObject() {
+		return $this->tealium;
+	}
+	public function getAccount($store) {
+		return Mage::getStoreConfig ( 'tealium_tags/general/account', $store );
+	}
+	public function getProfile($store) {
+		return Mage::getStoreConfig ( 'tealium_tags/general/profile', $store );
+	}
+	public function getEnv($store) {
+		return Mage::getStoreConfig ( 'tealium_tags/general/env', $store );
+	}
+	public function getUDOPath($store) {
+		return Mage::getStoreConfig ( 'tealium_tags/general/udo', $store );
+	}
+	public function getAPIEnabled($store) {
+		return Mage::getStoreConfig ( 'tealium_tags/general/api_enable', $store );
+	}
+	public function getIsExternalScript($store) {
+		return Mage::getStoreConfig ( 'tealium_tags/general/external_script', $store );
+	}
+	public function getExternalScriptType($store) {
+		$async = Mage::getStoreConfig ( 'tealium_tags/general/external_script_type', $store );
+		$scriptType = $async ? "async" : "sync";
+		return $scriptType;
+	}
+	public function getDiagnosticTag($store) {
+		if (Mage::getStoreConfig ( 'tealium_tags/general/diagnostic_enable', $store )) {
+			$utag_data = urlencode ( $this->tealium->render ( "json" ) );
+			$url = Mage::getStoreConfig ( 'tealium_tags/general/diagnostic_tag', $store ) . '?origin=server&user_agent=' . $_SERVER ['HTTP_USER_AGENT'] . '&data=' . $utag_data;
+			return '<img src="' . $url . '" style="display:none"/>';
+		}
+		return "";
+	}
+}
+	 
---
+++ lib/Tealium/TealiumInit.php
@@ -0,0 +1,301 @@
+<?php
+// TealiumInit.php var definition file
+// Replace $STRING or $ARRAY with your server side variable reference unique to that key 
+
+class TealiumData {
+	private static $store;
+	private static $page;
+	
+	public static function setStore($store){
+		TealiumData::$store = $store;
+	}
+	
+	public static function setPage($page){
+		TealiumData::$page = $page;
+	}
+	
+	public function getHome(){
+		$store = TealiumData::$store;
+		$page = TealiumData::$page;
+		
+		$outputArray = array();
+		$outputArray['site_region'] = Mage::app()->getLocale()->getLocaleCode() ?: "";
+		$outputArray['site_currency'] = $store->getCurrentCurrencyCode() ?: "";
+		$outputArray['page_name'] = $page->getLayout()->getBlock('head')->getTitle() ?: "";
+		$outputArray['page_type'] = $page->getTealiumType() ?: "";
+		
+		return $outputArray;
+	}
+	
+	public function getSearch(){
+		$store = TealiumData::$store;
+		$page = TealiumData::$page;
+		
+		$outputArray = array();
+	    $outputArray['site_region'] = Mage::app()->getLocale()->getLocaleCode() ?: "";
+	    $outputArray['site_currency'] = $store->getCurrentCurrencyCode() ?: ""; 
+	    $outputArray['page_name'] = "search results";
+	    $outputArray['page_type'] = "search";
+	    $outputArray['search_results'] = $page->getResultCount() . "" ?: "";
+	    $outputArray['search_keyword'] = $page->helper('catalogsearch')->getEscapedQueryText() ?: "";
+
+		return $outputArray;
+	}
+	
+	public function getCategory(){
+		$store = TealiumData::$store;
+		$page = TealiumData::$page;
+		
+		if ($page->getCurrentCategory()) {
+			$_category   = $page->getCurrentCategory();
+			$parent      = false;
+			$grandparent = false;
+		
+			// check for parent and grandparent
+			if ($_category->getParentId()) {
+				$parent = Mage::getModel('catalog/category')->load($_category->getParentId());
+		
+				if ($parent->getParentId()) {
+					$grandparent = Mage::getModel('catalog/category')->load($parent->getParentId());
+				}
+			}
+		
+			// Set the section and subcategory with parent and grandparent
+			if ($grandparent) {
+				$section     = $grandparent->getName();
+				$category    = $parent->getName();
+				$subcategory = $_category->getName();
+			} elseif ($parent) {
+				$section  = $parent->getName();
+				$category = $_category->getName();
+			} else {
+				$category = $_category->getName();
+			}
+		}
+		
+		$outputArray = array();
+		$outputArray['site_region'] = Mage::app()->getLocale()->getLocaleCode() ?: "";
+		$outputArray['site_currency'] = $store->getCurrentCurrencyCode() ?: "";
+		$outputArray['page_name'] = $_category ? ($_category->getName() ?: "") : "";
+		$outputArray['page_type'] = "category";
+		$outputArray['page_section_name'] = $section ?: "";
+		$outputArray['page_category_name'] = $category ?: "";
+		$outputArray['page_subcategory_name'] = $subcategory ?: "";
+		
+		return $outputArray;
+	}
+	
+	public function getProductPage(){
+		$store = TealiumData::$store;
+		$page = TealiumData::$page;
+		
+		$outputArray = array();
+		$outputArray['site_region'] = Mage::app()->getLocale()->getLocaleCode() ?: "";
+		$outputArray['site_currency'] = $store->getCurrentCurrencyCode() ?: "";
+		$outputArray['page_name'] = $_category ? ($_category->getName() ?: "") : "";
+		$outputArray['page_type'] = "product";
+		
+		// THE FOLLOWING NEEDS TO BE MATCHED ARRAYS (SAME NUMBER OF ELEMENTS)
+		if ( $page->getProduct() ) {
+			if ( !($outputArray['product_id'] = array( $page->getProduct()->getId() )) ){
+				$outputArray['product_id'] = array();
+			}
+			if ( !($outputArray['product_sku'] = array( $page->getProduct()->getSku() )) ){
+				$outputArray['product_sku'] = array();
+			}
+			if ( !($outputArray['product_name'] = array( $page->getProduct()->getName() )) ){
+				$outputArray['product_name'] = array();
+			}
+			if ( !($outputArray['product_brand'] = array( $page->getProduct()->getBrand() )) ){
+				$outputArray['product_brand'] = array();
+			}
+			if ( !($outputArray['product_unit_price'] = array( number_format($page->getProduct()->getSpecialPrice(), 2) )) ){
+				$outputArray['product_unit_price'] = array();
+			}
+			if ( !($outputArray['product_list_price'] = array( number_format($page->getProduct()->getPrice(), 2) )) ){
+				$outputArray['product_list_price'] = array();
+			}
+		}
+		else {
+			$outputArray['product_id'] = array();
+			$outputArray['product_sku'] = array();
+			$outputArray['product_name'] = array();
+			$outputArray['product_brand'] = array();
+			$outputArray['product_unit_price'] = array();
+			$outputArray['product_list_price'] = array();
+		}
+
+		if ( Mage::registry('current_category') ){
+			if ( Mage::registry('current_category')->getName() ){
+				$outputArray['product_category'] = array(Mage::registry('current_category')->getName());
+			}
+			else {
+				$outputArray['product_category'] = array();
+			}
+		}
+		else {
+			$outputArray['product_category'] = array();
+		}
+		
+		return $outputArray;
+	}
+	
+	public function getCartPage() {
+		$store = TealiumData::$store;
+		$page = TealiumData::$page;
+		
+		if (Mage::helper('checkout')) {
+			$quote = Mage::helper('checkout')->getQuote();
+			foreach ($quote->getAllVisibleItems() as $item) {
+				$checkout_ids[]    = $item->getProductId();
+				$checkout_skus[]   = $item->getSku();
+				$checkout_names[]  = $item->getName();
+				$checkout_qtys[]   = number_format($item->getQty(), 0, ".", "");
+				$checkout_prices[] = number_format($item->getPrice(), 2, ".", "");
+			}
+		}
+		
+		$outputArray = array();
+		$outputArray['site_region'] = Mage::app()->getLocale()->getLocaleCode() ?: "";
+		$outputArray['site_currency'] = $store->getCurrentCurrencyCode() ?: "";
+		$outputArray['page_name'] = $page->getLayout()->getBlock('head')->getTitle() ?: "";
+		$outputArray['page_type'] = "checkout";
+
+		// THE FOLLOWING NEEDS TO BE MATCHED ARRAYS (SAME NUMBER OF ELEMENTS)
+		$outputArray['product_id'] = $checkout_ids ?: array();
+		$outputArray['product_sku'] = $checkout_skus ?: array();
+		$outputArray['product_name'] = $checkout_names ?: array();
+		$outputArray['product_brand'] = array();
+		$outputArray['product_category'] = array();
+		$outputArray['product_quantity'] = $checkout_qtys ?: array();
+		$outputArray['product_unit_price'] = array();
+		$outputArray['product_list_price'] = $checkout_prices ?: array();
+		
+		return $outputArray;
+	}
+	
+	public function getOrderConfirmation(){
+		$store = TealiumData::$store;
+		$page = TealiumData::$page;
+		
+		if (Mage::getModel('sales/order')) {
+			$order = Mage::getModel('sales/order')->loadByIncrementId($page->getOrderId());
+			foreach ($order->getAllVisibleItems() as $item) {
+		
+				$ids[]           = $item->getProductId();
+				$skus[]          = $item->getSku();
+				$names[]         = $item->getName();
+				$qtys[]          = number_format($item->getQtyOrdered(), 0, ".", "");
+				$prices[]        = number_format($item->getPrice(), 2, ".", "");
+				$discount        = number_format($item->getDiscountAmount(), 2, ".", "");
+				$discounts[]     = $discount;
+				$applied_rules   = explode(",", $item->getAppliedRuleIds());
+				$discount_object = array();
+				foreach ($applied_rules as $rule) {
+					$quantity          = number_format(Mage::getModel('salesrule/rule')->load($rule)->getDiscountQty(), 0, ".", "");
+					$amount            = number_format(Mage::getModel('salesrule/rule')->load($rule)->getDiscountAmount(), 2, ".", "");
+					$type              = Mage::getModel('salesrule/rule')->load($rule)->getSimpleAction();
+					$discount_object[] = array("rule"		=>$rule,
+							"quantity"	=>$quantity,
+							"amount"	=>$amount,
+							"type"		=>$type);
+				}
+				$discount_quantity[] = array("product_id" => $item->getProductId(),
+						"total_discount"	=> $discount,
+						"discounts"		=> $discount_object);
+		
+			}
+		}
+		
+		$outputArray = array();
+
+		$outputArray['site_region'] = Mage::app()->getLocale()->getLocaleCode() ?: "";
+		$outputArray['site_currency'] = $store->getCurrentCurrencyCode() ?: "";
+		$outputArray['order_id'] = $order->getIncrementId() ?: "";
+		$outputArray['order_discount'] = number_format($order->getDiscountAmount(), 2, ".", "") ?: "";
+		$outputArray['order_subtotal'] = number_format($order->getSubtotal(), 2, ".", "") ?: "";
+		$outputArray['order_shipping'] = number_format($order->getShippingAmount(), 2, ".", "") ?: "";
+		$outputArray['order_tax'] = number_format($order->getTaxAmount(), 2, ".", "") ?: "";
+		$outputArray['order_payment_type'] = $order->getPayment() ? $order->getPayment()->getMethodInstance()->getTitle() : 'unknown';
+		$outputArray['order_total'] = number_format($order->getGrandTotal(), 2, ".", "") ?: "";
+		$outputArray['order_currency'] = $order->getOrderCurrencyCode() ?: "";
+		$outputArray['customer_email'] = $order->getCustomerEmail() ?: "";
+		$outputArray['product_id'] = $ids ?: array();
+		$outputArray['product_sku'] = $skus ?: array();
+		$outputArray['product_name'] = $names ?: array();
+		$outputArray['product_brand'] = array();
+		$outputArray['product_category'] = array();
+		$outputArray['product_unit_price'] = array();
+		$outputArray['product_list_price'] = $prices ?: array();
+		$outputArray['product_quantity'] = $qtys ?: array();
+		$outputArray['product_discount'] = $discounts ?: array();
+		$outputArray['product_discounts'] = $discount_quantity ?: array();
+		
+		return  $outputArray;
+	}
+	
+	public function getCustomerData(){
+		$store = TealiumData::$store;
+		$page = TealiumData::$page;
+		
+		if (Mage::getSingleton('customer/session')->isLoggedIn()) {
+			$customer       = Mage::getSingleton('customer/session')->getCustomer();
+			$customer_id    = $customer->getEntityId();
+			$customer_email = $customer->getEmail();
+			$groupId        = $customer->getGroupId();
+			$customer_type  = Mage::getModel('customer/group')->load($groupId)->getCode();
+		}
+		
+		$outputArray = array();
+
+		$outputArray['site_region'] = Mage::app()->getLocale()->getLocaleCode() ?: "";
+		$outputArray['site_currency'] = $store->getCurrentCurrencyCode() ?: "";
+		$outputArray['page_name'] = $page->getLayout()->getBlock('head')->getTitle() ?: "";
+		$outputArray['page_type'] = $page->getTealiumType() ?: "";
+		$outputArray['customer_id'] = $customer_id ?: "";
+		$outputArray['customer_email'] = $customer_email ?: "";
+		$outputArray['customer_type'] = $customer_type ?: "";
+		
+		return $outputArray;
+	}
+}
+
+
+TealiumData::setStore($data["store"]);
+TealiumData::setPage($data["page"]);
+
+
+$udoElements = array(
+    'Home' => function(){
+    	$tealiumData = new TealiumData();
+    	return $tealiumData->getHome();
+    },
+    'Search' => function(){
+    	$tealiumData = new TealiumData();
+    	return $tealiumData->getSearch();
+    },
+    'Category' => function(){
+    	$tealiumData = new TealiumData();
+    	return $tealiumData->getCategory();
+    },
+    'ProductPage' => function(){
+    	$tealiumData = new TealiumData();
+    	return $tealiumData->getProductPage();
+    },
+    'Cart' => function(){
+    	$tealiumData = new TealiumData();
+    	return $tealiumData->getCartPage();
+    },
+    'Confirmation' => function(){
+    	$tealiumData = new TealiumData();
+    	return $tealiumData->getOrderConfirmation();
+    },
+    'Customer' => function(){
+    	$tealiumData = new TealiumData();
+    	return $tealiumData->getCustomerData();
+    }
+);
+
+
+?> 
+
